3de938f840b45efe29943f48adbe0fb4916ab166
Noah Lanson <lanson.noah@gmail.com>
2021-05-02 17:40:55 +1000

    Trying to get web3.js to working in Angular

---
+++ Exchange/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { BlockchainService } from './services/blockchain.service';
+
+declare let window: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  async ngOnInit() {
+
+  }
+
+}
