162dbf8500d7080b5f5123f7ec812ddca30e39c4
Noah Lanson <lanson.noah@gmail.com>
2021-05-13 15:46:22 +1000

    Deploy to Ropsten

--- Token/truffle-config.js
+++ Token/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+let mnemonicPhrase = "<<ETH WALLET SEED PHRASE>>";
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,22 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    //Ropsten Testnet Deployment Settings
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://mainnet.infura.io/v3/f9285b64fbec4270946119d82a50dd26",
+          numberOfAddresses: 1,
+          shareNonce: true,
+          derivationPath: "m/44'/1'/0'/0/"
+        }),
+      network_id: '3',
+      skipDryRun: true
+    }
+    //End Ropsten
   },
   contracts_directory: './GoldStack/contracts/',
   contracts_build_directory: './GoldStack/abis/', //Smart contracts compile into the src/abis folder so that the react app can import it and use it.
